947fff652e217617ca07151b5aac072ba4916aea
max <mxbrkn@proton.me>
2022-06-20 00:36:02 +0300

    final

--- skillbox_task_4_3/task_4_3.cs
+++ skillbox_task_4_3/task_4_3.cs
@@ -1,41 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("*********** Добро пожаловать в игру УГАДАЙ ЧИСЛО ***********");
-Console.WriteLine();
-Console.Write("Будем отгадывать от 0 до ");
-
-int limit = int.Parse(Console.ReadLine());
+Console.Write("\nБудем отгадывать от 0 до ");
 
 Random random = new Random();
-
+int limit = int.Parse(Console.ReadLine());
 int guessNumber = random.Next(0, limit+1);
+int t = 0; // номер попытки 'try'
+int userNumber;
 
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Для выхода в любой момент нажмите Enter");
+Console.ForegroundColor = ConsoleColor.White;
 
-int t = 0; 
 while (true)
 {
-    Console.WriteLine();
-    Console.WriteLine("Ваше предположение: ");
+    Console.WriteLine("\nВаше предположение: ");
+    try 
+    {
+        userNumber = Convert.ToInt32(Console.ReadLine());
+    }
+    catch 
+    {
+        Console.WriteLine($"Было загадано {guessNumber}");
+        break;
+    }
+ 
 
-    //if (Console.ReadKey().Key == ConsoleKey.Enter)
-    //{
-    //    Console.WriteLine($"Было загадано {guessNumber}");
-    //    break;
-    //}
-
-    int userNumber = int.Parse(Console.ReadLine());
     t++;
     if (guessNumber > userNumber)
     {
-
         Console.WriteLine("Загаданное число больше");
+        
     }
     else if (guessNumber < userNumber)
     {
         Console.WriteLine("Загаданное число меньше");
+        
     }
     else
     {
         Console.WriteLine($"Вы угадали с {t}-й попытки!");
         break;
     }
+
+
 }
